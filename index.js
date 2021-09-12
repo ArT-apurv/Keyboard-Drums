19790b3c9ae6af8e0742c1e62c08e5cb78b9880b
ArT-apurv <89764232+ArT-apurv@users.noreply.github.com>
2021-09-12 13:47:57 +0530

    Play drums using keyboaed keys

--- index.js
+++ index.js
@@ -3,31 +3,31 @@ for (var i = 0; i < 7; i++) {
         var x = event.key;
         switch (x) {
             case "w":
-                var sound = new Audio("https://github.com/ArT-apurv/Keyboard-Drums/sounds/tom-1.mp3");
+                var sound = new Audio("https://art-apurv.github.io/Keyboard-Drums/sounds/tom-1.mp3");
                 sound.play();
                 break;
             case "a":
-                var sound = new Audio("https://github.com/ArT-apurv/Keyboard-Drums/sounds/tom-2.mp3");
+                var sound = new Audio("https://art-apurv.github.io/Keyboard-Drums/sounds/tom-2.mp3");
                 sound.play();
                 break;
             case "s":
-                var sound = new Audio("https://github.com/ArT-apurv/Keyboard-Drums/sounds/tom-3.mp3");
+                var sound = new Audio("https://art-apurv.github.io/Keyboard-Drums/sounds/tom-3.mp3");
                 sound.play();
                 break;
             case "d":
-                var sound = new Audio("https://github.com/ArT-apurv/Keyboard-Drums/sounds/tom-4.mp3");
+                var sound = new Audio("https://art-apurv.github.io/Keyboard-Drums/sounds/tom-4.mp3");
                 sound.play();
                 break;
             case "j":
-                var sound = new Audio("https://github.com/ArT-apurv/Keyboard-Drums/sounds/snare.mp3");
+                var sound = new Audio("https://art-apurv.github.io/Keyboard-Drums/sounds/snare.mp3");
                 sound.play();
                 break;
             case "k":
-                var sound = new Audio("https://github.com/ArT-apurv/Keyboard-Drums/sounds/crash.mp3");
+                var sound = new Audio("https://art-apurv.github.io/Keyboard-Drums/sounds/crash.mp3");
                 sound.play();
                 break;
             case "l":
-                var sound = new Audio("https://github.com/ArT-apurv/Keyboard-Drums/sounds/kick-bass.mp3");
+                var sound = new Audio("https://art-apurv.github.io/Keyboard-Drums/sounds/kick-bass.mp3");
                 sound.play();
                 break;
             default:console.log(x);
